16f14a2d98fb079a6bd8443d70c9cd45035271b3
Phoenix21Dragon <jasondippmann@gmail.com>
2022-07-10 18:13:05 +0200

    Implement Request of current weather

--- WeatherApplication/Program.cs
+++ WeatherApplication/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace WeatherApplication
 {
@@ -6,7 +8,14 @@ namespace WeatherApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string url = "https://api.openweathermap.org/data/2.5/weather?q=Freiberg&appid=81465b514607845ee21f943fc0f53acd&units=metric";
+            
+            HttpClient web = new HttpClient();
+            
+            string json = web.GetStringAsync(url).Result;
+
+            CurrentWeather currentweather = JsonConvert.DeserializeObject<CurrentWeather>(json);
+
         }
     }
 }
