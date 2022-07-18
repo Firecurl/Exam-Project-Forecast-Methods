2e80ae0730eb198d89ccd4a3e3f1ead17a2388e4
Firecurl <r.roth712@gmx.de>
2022-07-18 19:24:59 +0200

    add methods for reading csv file and convert into array

--- WeatherApplication/GeneralWeather.cs
+++ WeatherApplication/GeneralWeather.cs
@@ -9,11 +9,11 @@ namespace WeatherApplication
 
         // reading data from csv file 
 
-        private List<WeatherData> liste;
+        private List<WeatherData> list;
 
         public Table()
         {
-            liste = new List<WeatherData>();
+            list = new List<WeatherData>();
             string[] zeilen = File.ReadAllLines(@"General_Weather.csv");
             foreach(string zeile in zeilen)
             {
@@ -23,7 +23,32 @@ namespace WeatherApplication
                 int temperature = data[2];
                 int rain = data[3];
 
-                liste.Add(new WeatherData {Destination = destination, Season = season, Temperature = temperature, Raindays = rain});
+                list.Add(new WeatherData {Destination = destination, Season = season, Temperature = temperature, Raindays = rain});
+            }
+
+            // list in array, only get because should not be able to change something
+            public WeatherData [] getAll()
+            {
+                return list.ToArray();
+            }
+
+            // add further destinations
+            public void append (string destination, string season, int temperature, int rain)
+            {
+                list.Add(new WeatherData {Destination = destination, Season = season, Temperature = temperature, Raindays = rain});
+            }
+
+            // destructor for update the origin file
+
+            ~GeneralWeather()
+            {
+                string[] data = new string[list.Count];
+                for(int i=0; i< list.Count; i++)
+                {
+                    data[i] = list[i].Destination + ";" + list[i].Season ";" + list[i].Temperature ";" + list[i].Raindays;
+                }
+
+                File.WriteAllLines(@"General_Weather.csv", data);
             }
 
         }
